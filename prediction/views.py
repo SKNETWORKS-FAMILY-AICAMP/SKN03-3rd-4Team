25f786696663ee840a4a6b6b43cc11be93d05c97
mabasa-k <dkwcdr@gmail.com>
2024-09-23 21:41:11 +0900

    -model적용 테스트

--- prediction/views.py
+++ prediction/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import json
 import joblib
+import os
 from ml_models.preprocessing import preprocess_input
+# from tensorflow.keras.models import load_model
 
 # Create your views here.
 def prediction(request):
@@ -12,11 +14,25 @@ def prediction(request):
 def predict_churn(request):
     data = json.loads(request.body)
     
-    # input_data = preprocess_input(data)
+    # ml_input_data = preprocess_input(data)
+    # dl_input_data = preprocess_input(data)
 
-    # 모델 불러오기
-    # model = joblib.load('ml_model/VotingClassifier_best_model.pkl')
+    # 현재 파일의 경로를 기준으로 상대 경로를 계산
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    ml_model_path = os.path.join(BASE_DIR, 'ml_models', 'best_rf_model.pkl')
+
+
+    # 업로드 된 머신러닝 모델 불러오기
+    ml_model = joblib.load(ml_model_path)
+
     # 예측
-    # prediction = model.predict(input_data)
+    # prediction = ml_model.predict(ml_input_data)
+    
+    # 업로드 된 딥모델 파일 경로
+    # 모델 로드
+    # dl_model = load_model('media/models/your_model.h5')
+    # 예측
+    # predictions = dl_model.predict(dl_input_data)
+
 
     return JsonResponse({'result': 'ㅇ' })

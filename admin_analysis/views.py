6fc044f96d7a16f82be95d749f1150fe96d4b652
guna1218 <guna1218@gmail.com>
2024-09-24 12:44:07 +0900

    commit

--- admin_analysis/views.py
+++ admin_analysis/views.py
@@ -18,11 +18,41 @@ def admin_analysis(request):
             gender_male+=1
         else:
             gender_female+=1
+    churn_2023_yes=0
+    chrun_2023_No=0
+    for i in range(len(df['churn_2023'])):
+        if df['churn_2023'][i]=="Yes":
+            churn_2023_yes+=1
+        else:
+            chrun_2023_No+=1
+
+    churn_yes_tenure=[]
+    chrun_no_tenure=[]
+    for i in range(len(df['churn_2023'])):
+        if df['churn_2023'][i]=="Yes":
+            churn_yes_tenure.append(int(df['tenure_2023'][i]))
+        else:
+            chrun_no_tenure.append(int(df['tenure_2023'][i]))
+    yes_tenure_avg=sum(churn_yes_tenure)/len(churn_yes_tenure)
+    no_tenure_avg=sum(chrun_no_tenure)/len(chrun_no_tenure)
 
     return render(request, 'admin_analysis.html', {
         'gender_male': gender_male,
         'gender_female': gender_female,
+        'churn_yes':churn_2023_yes,
+        'churn_no':chrun_2023_No,
+        'yes_tenure_avg':yes_tenure_avg,
+        'no_tenure_avg':no_tenure_avg
     })
 
-
+file_path = r'C:/dev/github/SKN03-3rd-4Team/Churn_Data_-_Final_Version2.csv'
+df = pd.read_csv(file_path)
+churn_yes_tenure=[]
+chrun_no_tenure=[]
+for i in range(len(df['churn_2023'])):
+    if df['churn_2023'][i]=="Yes":
+        churn_yes_tenure.append(int(df['tenure_2023'][i]))
+    else:
+        chrun_no_tenure.append(int(df['tenure_2023'][i]))
+print(len(df['churn_2023']))
 
